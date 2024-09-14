96456f14db06c7577c2af7e074e8e68c77d7e925
lhb1936319431 <1936319431@qq.com>
2024-09-14 12:20:34 +0800

    v0.0.8

--- site.config.ts
+++ site.config.ts
@@ -7,7 +7,7 @@ export default defineSiteConfig({
   subtitle: '一个人的世界',
   author: {
     name: 'lhb-初入江湖',
-    avatar: '/avatar.png',
+    avatar: '/logo.gif',
     status:{
       emoji: '💰',
       message: '修行中...',
@@ -15,7 +15,7 @@ export default defineSiteConfig({
   },
   description: '记录生活点滴，分享技术心得.',
 
-  favicon: '/avatar.png',
+  favicon: '/logo.gif',
 
   social: [
     {
